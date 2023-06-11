9f3ce44a3a4d9cb31ce5e89dc4b95913cb920c70
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@gmail.com>
2023-06-11 15:45:44 +0300

    Added: Navigation Animations

--- js/Scrolling.js
+++ js/Scrolling.js
@@ -12,10 +12,24 @@ const hiddenElements = document.querySelectorAll('.aein-move-hidden');
 
 
 
+let navigationItems = document.querySelectorAll("header .links-cont .links ul li a");
 let skillsSection = document.querySelector("#Skills");
 let skillItems = document.querySelectorAll("#Skills .tech-container .skills-container .skill .img-cont");
+
+
+
 window.onscroll = function () {
-  // Skills Animate Width
+    // Navigation Animations 
+    navigationItems.forEach(Item => {
+        let hrefValue = Item.getAttribute("href");
+        let thisSection = document.querySelector(`${hrefValue}`);
+        if (thisSection.offsetTop - 200 <= window.scrollY && window.scrollY <= thisSection.nextElementSibling.offsetTop -200) {
+            Item.parentElement.classList.add("active");
+        } else {
+            Item.parentElement.classList.remove("active");
+        }
+    });
+    // Skills Animate Width
     if (window.scrollY >= skillsSection.offsetTop - 500) {
         skillItems.forEach((skill , i) => {
         skill.style.setProperty("--transform-delay", `${(i> 2)? (i+1)*100 : (i+1)*500 }ms`);    
